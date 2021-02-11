6cda214835f1749265511a6a9138d58daed248b4
GabrielHdezAlv <gabrielhdezalvarez@gmail.com>
2021-02-11 13:36:08 +0000

    Plugins Instalados

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+const jsdoc = require('gulp-jsdoc3');
+const eslint = require('gulp-eslint');
 
 sass.compiler = require('node-sass');
 
@@ -18,6 +20,27 @@ gulp.task('sass:watch', function () {
   gulp.watch('./css/*.scss', gulp.series(['sass']));
 });
 
+//JsDoc
+gulp.task('doc', function (cb) {
+  gulp.src(['./js/*.js'], { read: false }).pipe(jsdoc(cb));
+});
+
+//EsLint
+gulp.task('eslint', () => {
+  return (
+    src(['./js/*.js'])
+      // eslint() attaches the lint output to the "eslint" property
+      // of the file object so it can be used by other modules.
+      .pipe(eslint())
+      // eslint.format() outputs the lint results to the console.
+      // Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.format())
+      // To have the process exit with an error code (1) on
+      // lint error, return the stream and pipe to failAfterError last.
+      .pipe(eslint.failAfterError())
+  );
+});
+
 // Static Server + watching scss/html files
 gulp.task(
   'serve',
@@ -27,6 +50,8 @@ gulp.task(
     });
 
     gulp.watch('./css/*.scss', gulp.series(['sass']));
+    gulp.watch('./js/*.js', gulp.series(['doc']));
+    gulp.watch('./js/*.js', gulp.series(['eslint']));
     gulp.watch('./index.html').on('change', browserSync.reload);
     gulp.watch('./js/*.js').on('change', browserSync.reload);
   })
